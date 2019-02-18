e1adfeada4fbbd32e2d3f7e764ea4d357f55f9b0
clund12 <c.lund@protonmail.com>
2019-02-18 13:30:34 -0500

    added all 1 MeV macro files so that histogram data does not need to be transferred between computers

---
+++ Track_1MeV_scorer2_nitrogens.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+for i in {1..10000};
+do /home/chris/geant4/work/spisTracks/build/spisTracks Track_1MeV_scorer2_nitrogens.mac /home/chris/geant4/work/Data/Tracks/Neutrons/1MeV/Nitrogens $i > /dev/null;
+done
