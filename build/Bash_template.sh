38eac08b74a575735fb7426f2fdd7ddd88760bb0
clund12 <c.lund@protonmail.com>
2019-03-14 18:53:19 -0400

    Finished aAV calculation script. Need to integrate it into Geant4 code

--- build/Bash_template.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for i in {trackRange};
-do /home/chris/geant4/work/spisTracks/build/spisTracks {macroName} {outputName} $i > /dev/null;
-done
--- build/test_core1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for i in {6..10};
-do /home/chris/geant4/work/spisTracks/build/spisTracks test.mac /home/chris/geant4/work/Data/Tracks/Neutrons/Testing $i;
-done
